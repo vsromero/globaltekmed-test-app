7df3b98178faae7a5e904bb4a6b354e8cb6d0ff3
Victor Sandoval <victorsandovalromero@outlook.com>
2024-07-29 14:14:56 -0700

    remove ssl_certs

--- backend/utils.py
+++ backend/utils.py
@@ -2,6 +2,6 @@ from pymongo import MongoClient
 import ssl
 
 def get_db_handle():
-  client = MongoClient('mongodb+srv://viccsr1:5CbNIQSsLPyvuiRS@cluster0.87pjexr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', ssl_cert_reqs=ssl.CERT_NONE)
+  client = MongoClient('mongodb+srv://viccsr1:5CbNIQSsLPyvuiRS@cluster0.87pjexr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
   db_handle = client['db_name']
   return db_handle, client
