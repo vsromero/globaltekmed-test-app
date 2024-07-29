36cdcc1ba35226fda1790f587b7cf1fa6a1802c7
Victor Sandoval <victorsandovalromero@outlook.com>
2024-07-29 14:30:59 -0700

    remove path allow origin

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -127,5 +127,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
-    'http://34.70.220.139:3000/',
+    'http://34.70.220.139:3000',
 ]
