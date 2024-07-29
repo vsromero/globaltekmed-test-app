aa71f872c0258dbfaa86d3a05c9a8963a9ce5012
Victor Sandoval <victorsandovalromero@outlook.com>
2024-07-29 11:34:44 -0700

    ssl fix

--- backend/utils.py
+++ backend/utils.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
+import ssl
 
 def get_db_handle():
-  client = MongoClient('mongodb+srv://viccsr1:5CbNIQSsLPyvuiRS@cluster0.87pjexr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+  client = MongoClient('mongodb+srv://viccsr1:5CbNIQSsLPyvuiRS@cluster0.87pjexr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', ssl_cert_reqs=ssl.CERT_NONE)
   db_handle = client['db_name']
   return db_handle, client
